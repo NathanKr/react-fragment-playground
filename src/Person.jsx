ac6b3ca81100a180a125de4b92f489b0faca84a2
Nathan Krasney <natankrasney@gmail.com>
2021-11-09 11:32:37 +0200

    use react.createElement

--- src/Person.jsx
+++ src/Person.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
+// const Person = ({ firstName, lastName }) => {
+//   return (
+//     <>
+//       <p>first name : {firstName}</p>
+//       <p>last name : {lastName}</p>
+//     </>
+//   );
+// };
+
+("use strict");
+
 const Person = ({ firstName, lastName }) => {
-  return (
-    <>
-      <p>first name : {firstName}</p>
-      <p>last name : {lastName}</p>
-    </>
+  return /*#__PURE__*/ React.createElement(
+    React.Fragment,
+    null,
+    /*#__PURE__*/ React.createElement("p", null, "first name : ", firstName),
+    /*#__PURE__*/ React.createElement("p", null, "last name : ", lastName)
   );
 };
 
